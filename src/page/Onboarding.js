7725f5b1c01d45b466f56e1e9594f9b7cbb57148
Ricky Prahatama <ricky.prahatama@gmail.com>
2018-06-21 21:22:49 +0700

    Added vector icon for Android

--- src/page/Onboarding.js
+++ src/page/Onboarding.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import Onboarding from 'react-native-onboarding-swiper';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Image } from 'react-native';
+import { StyleSheet, Text, Image } from 'react-native';
 
 export default class OnboardingPage extends React.Component {  
   render() {
@@ -14,30 +15,31 @@ export default class OnboardingPage extends React.Component {
         pages={[
           {
             backgroundColor: backgroundColor,
-            image: <Image source={require('../assets/images/onboarding-1.png')} />,
+            image: <Image source={require('../assets/images/onboarding/onboarding-1.png')} />,
             title: 'Lihat dan Bayar Tagihan',
             subtitle: 'Lunasi tagihan listrik, air, telepon, hingga internet tanpa ribet langsung di ponsel'
           },
           {
             backgroundColor: backgroundColor,
-            image: <Image source={require('../assets/images/onboarding-2.png')} />,
+            image: <Image source={require('../assets/images/onboarding/onboarding-2.png')} />,
             title: 'Selalu Up-to-date',
             subtitle: 'Dapatkan berita dan informasi terbaru seputar hunian Anda dalam newsfeed'
           },
           {
             backgroundColor: backgroundColor,
-            image: <Image source={require('../assets/images/onboarding-3.png')} />,
+            image: <Image source={require('../assets/images/onboarding/onboarding-3.png')} />,
             title: 'Belanja Kebutuhan Harian',
             subtitle: 'Praktis belanja makanan, minuman, kebutuhan dapur, dan kebutuhan harian lainnya'
           },
           {
             backgroundColor: backgroundColor,
-            image: <Image source={require('../assets/images/onboarding-4.png')} />,
+            image: <Image source={require('../assets/images/onboarding/onboarding-4.png')} />,
             title: 'Booking Fasilitas Umum',
             subtitle: 'Booking lapangan tenis, lapangan basket, dan balai warga makin mudah'
           }
         ]}
         skipLabel="Lewati"
+        nextLabel={<Icon name="arrow-forward" size={24} color="#ffffff" />}
         bottomBarHighlight={false}
         imageContainerStyles={{
           paddingBottom: 100
