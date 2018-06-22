23bb491f4bca4adec98ba5d29ea1b044ffe0d0a3
Ricky Prahatama <ricky.prahatama@gmail.com>
2018-06-22 07:54:59 +0700

    Login Page

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import OnboardingPage from './src/page/Onboarding';
+import LoginPage from './src/page/Login';
 
 export default class App extends React.Component {
   render() {
     return (
-      <OnboardingPage />
+      <LoginPage />
     );
   }
 }
---
+++ src/lib/styles.js
@@ -0,0 +1,4 @@
+export const colors = {
+  mainColor: '#F19100',
+  shadowColor: '#000000'
+}
