2416ef1e2fab4721264d52bc068a3f512f5e6aa6
Ricky Prahatama <ricky.prahatama@gmail.com>
2019-02-08 08:41:30 +0700

    Add change password

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,4 +1,5 @@
 import { env } from '../lib/environment';
+import { appStorage, storageConst } from '../lib/storage';
 
 // get building list
 export function getBuildingList(){
@@ -22,5 +23,43 @@ export function getBuildingList(){
       .catch(error => {
         reject(error);
       })
+  });
+}
+
+// change password
+export function changePassword(old_password, new_password1, new_password2){
+  return new Promise(async (resolve, reject) => {
+    try {
+      let userToken = await appStorage.getItem(storageConst.user);
+      userToken = JSON.parse(userToken).token;
+      
+      fetch(`${env.ENDPOINT}/api/auth/change_password`, {
+        method: 'POST',
+        headers: new Headers({
+          'Accept-Encoding': 'application/json',
+          'Content-Type': 'application/json',
+          'Token': userToken
+        }),
+        body: JSON.stringify({
+          old_password: old_password,
+          new_password1: new_password1,
+          new_password2: new_password2
+        })
+      })
+      .then(response => {
+        response.json().then(responseBody => {
+          if(response.status === 200){
+            resolve(responseBody);
+          } else {
+            reject(responseBody);
+          }
+        })
+      })
+      .catch(error => {
+        reject(error);
+      })
+    } catch(err) {
+      reject(err);
+    }
   });
 }
--- src/screens/Setting.js
+++ src/screens/Setting.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet, Alert, Image } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Alert, Image, TextInput, TouchableOpacity } from 'react-native';
 
 import FormLabel from '../components/Form/Label';
 
 import { colors } from '../lib/styles';
 import { appStorage, storageConst } from '../lib/storage';
+import { changePassword } from '../requests';
 
 export default class SettingScreen extends React.Component {
   constructor(props){
@@ -14,7 +15,12 @@ export default class SettingScreen extends React.Component {
       name: null,
       email: null,
       unit: null,
-      phone: null
+      phone: null,
+      site: null,
+      oldPassword: '',
+      newPassword: '',
+      newPasswordConfirmation: '',
+      formSubmitting: false,
     }
   }
   
@@ -33,7 +39,8 @@ export default class SettingScreen extends React.Component {
         name: currentUser.name,
         email: currentUser.email,
         phone: currentUser.phone,
-        unit: currentUser.unit
+        unit: currentUser.unit,
+        site: currentUser.building_name,
       })
     } catch (error) {
       Alert.alert('Gagal Mengambil Detail User', JSON.stringify(error))
@@ -43,6 +50,30 @@ export default class SettingScreen extends React.Component {
   
   componentDidMount(){
     this.getCurrentUser();
+  }
+  
+  onFormSubmit = async () => {
+    const { formSubmitting, newPassword, newPasswordConfirmation, oldPassword } = this.state;
+    const { navigate } = this.props.navigation;
+    
+    if(!formSubmitting){
+      this.setState({ formSubmitting: true });
+      try {
+        const changePasswordResult = await changePassword(oldPassword, newPassword, newPasswordConfirmation);
+        if(changePasswordResult.data){
+          Alert.alert('Ganti Password Berhasil', changePasswordResult.data, [
+            {text: 'Ok', onPress: () => { console.log(navigate); navigate('Logout') }}
+          ]);
+        } else {
+          Alert.alert('Error', changePasswordResult.message);
+        }
+      }catch(err){
+        this.setState({formSubmitting: false});
+        Alert.alert('Error', err.message);
+      }
+    } else {
+      return false;
+    }    
   }
   
   render(){
@@ -59,12 +90,53 @@ export default class SettingScreen extends React.Component {
             <Text style={styles.pageBodyText}>{ this.state.unit }</Text>
           </View>
           <View style={styles.formSet}>
+            <FormLabel textColor="#999999" text="Site" />
+            <Text style={styles.pageBodyText}>{ this.state.site }</Text>
+          </View>
+          <View style={styles.formSet}>
             <FormLabel textColor="#999999" text="Nomor HP" />
             <Text style={styles.pageBodyText}>{ this.state.phone }</Text>
           </View>
           <View style={styles.formSet}>
             <FormLabel textColor="#999999" text="Email" />
             <Text style={styles.pageBodyText}>{ this.state.email }</Text>
+          </View>
+          <View style={{borderTopWidth: 1, borderTopColor: '#aaaaaa', marginBottom: 16}}></View>
+          <View style={styles.formSet}>
+            <Text style={{fontSize: 16}}>Ganti Password</Text>
+            <FormLabel textColor="#999999" text="password lama" />
+            <TextInput 
+              secureTextEntry={true} 
+              style={styles.textInput} 
+              onChangeText={ (text) => this.setState({oldPassword: text}) }
+              value={this.state.oldPassword}
+            />
+          </View>
+          <View style={styles.formSet}>
+            <FormLabel textColor="#999999" text="password baru" />
+            <TextInput 
+              secureTextEntry={true} 
+              style={styles.textInput} 
+              onChangeText={ (text) => this.setState({newPassword: text}) }
+              value={this.state.newPassword}
+            />
+          </View>
+          <View style={styles.formSet}>
+            <FormLabel textColor="#999999" text="konfirmasi password baru" />
+            <TextInput 
+              secureTextEntry={true} 
+              style={styles.textInput} 
+              onChangeText={ (text) => this.setState({newPasswordConfirmation: text}) }
+              value={this.state.newPasswordConfirmation}
+            />
+          </View>
+          <View style={styles.formSet}>
+            <TouchableOpacity 
+              style={styles.formButton}
+              onPress={() => this.onFormSubmit()}
+            >
+              <Text style={{ color: colors.primary, fontWeight: 'bold' }}>{ this.state.formSubmitting ? 'MENGIRIM' : 'GANTI PASSWORD' }</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
@@ -93,5 +165,18 @@ const styles = StyleSheet.create({
   },
   profilePicture: {
     marginBottom: 15
-  }
+  },
+  formButton: {
+    backgroundColor: '#FFFFFF',
+    width: '100%',
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 2,
+    elevation: 2
+  },
+  textInput: {
+    color: "#999999",
+    borderColor: "#FFFFFF",
+    fontSize: 16
+  },
 })
